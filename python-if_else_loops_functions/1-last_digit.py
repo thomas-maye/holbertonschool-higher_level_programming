db621a826e89ae080c5e89300f92f43268b3c180
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-03 15:58:45 +0200

    Fix my code

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 first = "Last digit of"
+
 if number < 0:
-    last_digit = -number % 10
+    last_digit = -(-number % 10)
 else:
     last_digit = number % 10
 
