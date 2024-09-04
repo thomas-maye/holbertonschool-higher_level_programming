33f1c233dca3ff5868015552b23d5ed275d625e0
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-04 16:08:03 +0200

    Write a program that prints the result of the addition of all arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argv = sys.argv
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("0")
+    else:
+        total_sum = 0
+        for i in range(1, argc + 1):
+            total_sum = total_sum + int(argv[i])
+        print(total_sum)
