a69fbf650207dd233fd50dbed7aa5b78090e90c0
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-10 15:04:50 +0200

    Delete the comments

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list, number):
     return list(map(lambda x: x * number, my_list))
-# the function lambda x: x * number multiplies each element
-# of my_list by number
