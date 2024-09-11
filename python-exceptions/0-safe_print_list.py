376929017313f7b8f81affde9239e5244f0370ce
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-11 10:57:31 +0200

    Try to fix my code in order to get max points

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
         print()
-        return i + 1
-
+        return x
+    
     except IndexError:
         print()
         return i
