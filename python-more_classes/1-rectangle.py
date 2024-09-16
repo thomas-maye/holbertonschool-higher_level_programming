40e4af5faca547bcfbb8081639573ccd97ce3c43
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-16 14:25:42 +0200

    Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,48 @@
 #!/usr/bin/python3
+"""Function that defines a rectangle"""
+
+
+class Rectangle:
+    """Create a class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes width and height of rectangle
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for width of rectangle
+        Returns:
+            width of rectangle
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """Getter for height of rectangle
+        Returns:
+            height of rectangle
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Setter for width of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Setter for height of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
