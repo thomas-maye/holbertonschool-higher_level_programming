274eb8606bc91e6725526c09cfcc3077eb572c60
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-16 13:29:49 +0200

    Write a class Square that defines a square by: (based on 5-square.py)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,7 +8,7 @@ class Square:
     Attributes:
         __size (int): size of a square's side
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0 , position=(0, 0)):
         """Initializes a square
 
         Args:
@@ -18,16 +18,25 @@ class Square:
             None
         """
         self.__size = size
+        self.__position = position
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        if (not isinstance(position, tuple) or len(position) != 2 or
+                not all(isinstance(num, int) for num in position) or
+                not all(num >= 0 for num in position)):
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
     def size(self):
         """int: size of a square's side"""
         return self.__size
+    
+    @property
+    def position(self):
+        """tuple: position of the square"""
+        return self.__position
 
     @size.setter
     def size(self, value):
@@ -36,6 +45,14 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Calculates the area of a square
@@ -50,5 +67,6 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            print("\n" * self.__position[1], end="")
             for i in range(self.__size):
-                print("#" * self.__size)
+                print("_" * self.__position[0] + "#" * self.__size)
