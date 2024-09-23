93bc103b5d57667c5b45f15bd580ae58653164fc
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-23 11:54:33 +0200

    Write a function that returns the list of available attributes and methods of an object

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""A class MyList that inherits from list"""
+
+
+class MyList(list):
+    """A class MyList that inherits from list"""
+    def print_sorted(self):
+        """Prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""A function that returns True if the object is
+exactly an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is exactly an instance
+    of the specified class
+    Args:
+        obj: object
+        a_class: class
+    Returns:
+        True if obj is exactly an instance of a_class
+        False otherwise
+    """
+    if type(obj) is a_class:
+        return True
+    return False
