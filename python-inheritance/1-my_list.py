e177abe667e7400b7bc3a2c9da67693d69b86831
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-23 13:21:27 +0200

    Add Unit Test.text file

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,6 +4,10 @@
 
 class MyList(list):
     """A class MyList that inherits from the list"""
+    def __init__(self):
+        """Initializes the MyList object"""
+        super().__init__()
+
     def print_sorted(self):
         """Prints the list, but sorted (ascending sort)"""
         print(sorted(self))
