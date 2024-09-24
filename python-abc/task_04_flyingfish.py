38e91f7d02a3cc8da647c713ab621b3b8729d541
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-24 15:54:20 +0200

    Create a Bird and Fish classes, and a subclass FlyingBird and test mro in the main

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+"""A module that defines a class FlyingFish"""
+
+
+class Fish:
+    def swim(self):
+        """A method that prints : The fish is swimming"""
+        print("The fish is swimming")
+
+    def habitat(self):
+        """A method that prints : The fish lives in water"""
+        print("The fish lives in water")
+
+
+class Bird:
+    def fly(self):
+        """A method that prints : The bird is flying"""
+        print("The bird is flying")
+
+    def habitat(self):
+        """A method that prints : The bird lives in the sky"""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    def fly(self):
+        """A method that prints : The flying fish is soaring!"""
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """A method that prints : The flying fish is swimming!"""
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """A method that prints : The flying fish lives both in
+        water and sky"""
+        print("The flying fish lives both in water and sky!")
