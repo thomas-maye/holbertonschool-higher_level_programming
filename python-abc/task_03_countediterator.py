f6b5995e9d98c92d21b7a1ee4bfb06c93c0d39ae
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-24 15:22:30 +0200

    Create a CountedIterator class to count all iterations during the execution of the program

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,47 @@
 #!/usr/bin/env python3
+"""A module that defines keeping track of the number of iterations"""
+
+
+class CountedIterator:
+    """A class that defines keeping track of the number of iterations"""
+
+    def __init__(self, iterable, count=0):
+        """A method that initializes the class"""
+        self.count = count
+        self.iterable = iterable
+        self.index = 0
+
+    def __iter__(self):
+        """
+        A method that returns the iterator object
+
+        Returns:
+            self: the iterator object
+        """
+        return self
+
+    def __next__(self):
+        """
+        A method that returns the next item in the iteration
+
+        Returns:
+            self.iterable[self.index - 1]: the next item in the iteration
+
+        Raises:
+            StopIteration: when the iteration is done
+        """
+        if self.index < len(self.iterable):
+            self.index += 1
+            self.count += 1
+            return self.iterable[self.index - 1]
+        else:
+            raise StopIteration
+
+    def get_count(self):
+        """
+        A method that returns the number of iterations
+
+        Returns:
+            self.index: the number of iterations
+        """
+        return self.index
