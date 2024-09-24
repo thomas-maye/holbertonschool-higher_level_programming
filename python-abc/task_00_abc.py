e77dc0bab516dca0ce62cd30613baa1635f69feb
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-24 10:59:34 +0200

    Create a class Animal, and two subclasses Dog and Cat, and an abstract method sound for Animal, and a method sound for Dog and Cat

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+from abc import ABC, abstractmethod
+"""importing ABC and abstractmethod from abc module"""
+
+
+class Animal(ABC):
+    """Create an Animal class"""
+    @abstractmethod
+    def sound(self):
+        """Create an sound abstract method"""
+        pass
+
+class Dog(Animal):
+    """ Create a Dog Class"""
+    def sound(self):
+        """Create a sound method for Dog"""
+        return "Bark"
+    
+class cat(Animal):
+    """Create a Cqat Class"""
+    def sound(self):
+        """Create a sound method for Cat"""
+        return "Meow"
