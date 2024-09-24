db721c019727c60a7c35d7dfb1c4f4a02532a44e
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-24 18:55:22 +0200

    correcting

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
-"""A module that create a mystical Dragon"""
+"""A module that creates a mystical Dragon"""
 
 
 class SwimMixin:
     def swim(self):
-        """A method that prints : The dragon is swimming"""
+        """A method that prints: The creature swims!"""
         print("The creature swims!")
 
 
 class FlyMixin:
     def fly(self):
-        """A method that prints : The dragon is flying"""
+        """A method that prints: The creature flies!"""
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
     def roar(self):
-        """A method that prints : The dragon is roaring"""
-        print("The creature roars!")
+        """A method that prints: The dragon roars!"""
+        print("The dragon roars!")
+
+
+if __name__ == "__main__":
+    draco = Dragon()
+    draco.fly()
+    draco.swim()
+    draco.roar()
