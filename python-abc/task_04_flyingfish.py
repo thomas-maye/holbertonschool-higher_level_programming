c2d20822459371a9b613d2b9603a83190270ff96
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-24 16:06:11 +0200

    Documentation style

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,37 +1,53 @@
 #!/usr/bin/env python3
-"""A module that defines a class FlyingFish"""
+"""
+A module that defines a class FlyingFish
+"""
 
 
 class Fish:
     def swim(self):
-        """A method that prints : The fish is swimming"""
+        """
+        A method that prints : The fish is swimming
+        """
         print("The fish is swimming")
 
     def habitat(self):
-        """A method that prints : The fish lives in water"""
+        """
+        A method that prints : The fish lives in water
+        """
         print("The fish lives in water")
 
 
 class Bird:
     def fly(self):
-        """A method that prints : The bird is flying"""
+        """
+        A method that prints : The bird is flying
+        """
         print("The bird is flying")
 
     def habitat(self):
-        """A method that prints : The bird lives in the sky"""
+        """
+        A method that prints : The bird lives in the sky
+        """
         print("The bird lives in the sky")
 
 
 class FlyingFish(Fish, Bird):
-    def swim(self):
-        """A method that prints : The flying fish is swimming!"""
-        print("The flying fish is swimming!")
-    
     def fly(self):
-        """A method that prints : The flying fish is soaring!"""
+        """
+        A method that prints : The flying fish is soaring!
+        """
         print("The flying fish is soaring!")
 
+    def swim(self):
+        """
+        A method that prints : The flying fish is swimming!
+        """
+        print("The flying fish is swimming!")
+
     def habitat(self):
-        """A method that prints : The flying fish lives both in
-        water and sky"""
+        """
+        A method that prints : The flying fish lives both in
+        water and sky
+        """
         print("The flying fish lives both in water and sky!")
