4e30ab5c6456da81a9a0b017f63448f9cfcc6302
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-24 18:43:52 +0200

    Create two mixin swim and fly for the Dragon which called draco

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""A module that create a mystical Dragon"""
+
+
+class SwimMixin:
+    def swim(self):
+        """A method that prints : The dragon is swimming"""
+        print("The creature swims!")
+
+
+class FlyMixin:
+    def fly(self):
+        """A method that prints : The dragon is flying"""
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        """A method that prints : The dragon is roaring"""
+        print("The creature roars!")
+
+if __name__ == "__main__":
+    draco = Dragon()
+    draco.fly()
+    draco.swim()
+    draco.roar()
