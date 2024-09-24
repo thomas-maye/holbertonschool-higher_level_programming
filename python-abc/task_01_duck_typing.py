599d90cbaebaf9cad7dfd79cb40aed6abf18d9ba
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-24 11:19:21 +0200

    Create a Shape abstract class, and two subclasses Circle and Rectangle, and two method to calculate the area and perimeter for each classes.

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,1 +1,48 @@
 #!/usr/bin/env python3
+"""Shapes Interfaces and Duck Typing."""
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    """Create Shape abstract class."""
+    @abstractmethod
+    def area(self):
+        """Create an area abstract method."""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Create a perimeter abstract method."""
+        pass
+
+
+class Circle(Shape):
+    """Create a Circle class."""
+    def __init__(self, radius):
+        """Create a constructor for Circle class."""
+        self.radius = radius
+
+    def area(self):
+        """Create an area method for Circle class."""
+        return pi * self.radius ** 2
+
+    def perimeter(self):
+        """Create a perimeter method for Circle class."""
+        return 2 * pi * self.radius
+
+
+class Rectangle(Shape):
+    """Create a Recatngle class."""
+    def __init__(self, width, height):
+        """Create a constructor for Rectangle class."""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Create an area method for Rectangle class."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Create a perimeter method for Rectangle class."""
+        return 2 * (self.width + self.height)
