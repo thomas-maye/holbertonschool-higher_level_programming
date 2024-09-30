f3eba30c0c422b8e602b68001a9ed3812d65053b
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-30 10:25:10 +0200

    Write a function that writes a string to a text file (UTF8) and returns the number of characters.

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Module for write_file method."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8).
+
+    Args:
+        filename (str): The file to write.
+        text (str): The text to write.
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
