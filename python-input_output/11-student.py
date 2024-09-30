69db706cba80fc1412378ed80001c659ba3d25fa
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-30 13:55:12 +0200

    Write a class Student that defines a student.

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+"""A class Student that defines a student"""
+
+
+class Student:
+    """A class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student instance
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+
+        Returns:
+            None
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list): A list of strings
+
+        Returns:
+            dict: A dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+
+        Args:
+            json (dict): A dictionary
+
+        Returns:
+            None
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
