a36bfa204960ea77fc39f983c0fb0abc79cf3846
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-30 14:16:08 +0200

    Write a class Student that defines a student by: (based on 10-student.py)

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Pascal's Triangle a function that returns a list of lists
+of integers representing the Pascal's triangle of a given number
+of rows."""
+
+
+def pascal_triangle(n):
+    """Function that returns a list of lists of integers
+    representing the Pascal's triangle of a given number of rows."""
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
