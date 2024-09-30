17b56d5b168cc8c3ca0337b3441553beee172418
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-30 10:41:07 +0200

    Write a function that returns the JSON representation of an object (string).

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Module to write an object to a text file using
+JSON representation of an objetc."""
+import json as j
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object.
+
+    Args:
+        my_obj (obj): The object to represent.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return j.dumps(my_obj)
