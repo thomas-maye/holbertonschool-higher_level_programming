aaefc741514135773ffae6c4f0e2f7a5bdabeadd
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-30 13:33:28 +0200

    Write a class Student that defines a student.

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,7 +5,16 @@
 class Student:
     """A class Student that defines a student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes a student instance"""
+        """Initializes a student instance
+        
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+
+        Returns:
+            None
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
