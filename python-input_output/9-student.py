e001e223ccb763b366590f082a30dd5edfb311e2
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-30 13:08:50 +0200

    Write a class Student that defines a student.

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""A class Student that defines a student"""
+
+
+class Student:
+    """A class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
