abc2adf861f00d687fcc76e49bfbcd293a36ba8f
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-10-10 15:58:18 +0200

    Change my code, and Add status

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -29,11 +29,17 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
                 }
             self.wfile.write(json.dumps(info).encode("utf-8"))
 
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(b'OK')
+
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found\n")
+            self.wfile.write(b"Endpoint not found\n")
 
 
 PORT = 8000
