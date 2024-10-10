ff4cf0c2caa76a023230096b09b672316adcb152
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-10-10 16:52:23 +0200

    replace info and data

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -19,20 +19,21 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b"Hello, this is a simple API!\n")
 
         elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
-            data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode("utf-8"))
 
         elif self.path == "/info":
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
             info = {
                 "version": "1.0",
                 "description": "A simple API built with http.server"
                 }
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+
             self.wfile.write(json.dumps(info).encode("utf-8"))
 
         elif self.path == "/status":
