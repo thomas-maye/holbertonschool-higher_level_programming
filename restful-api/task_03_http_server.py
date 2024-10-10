4fbbdaff52a5d7d0b11e9ddecb232113cae42287
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-10-10 16:33:54 +0200

    transfer in uppercase

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -11,20 +11,20 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         """Handle GET requests"""
         if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!\n")
 
         elif self.path == "/data":
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode("utf-8"))
 
         elif self.path == "/info":
             self.send_response(200)
-            self.send_header("Content-type", "application/json")
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
             info = {
                 "version": "1.0",
@@ -34,13 +34,13 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
 
         elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-type", "application/json")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b'OK')
 
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Endpoint not found\n")
 
