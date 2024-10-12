394549eb7d48fc462128f25982dc61b9ae36e289
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-10-12 19:52:35 +0200

    test

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,14 +2,14 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# In-memory users dictionary to store user data
+# Dictionary to store user details.
 users = {}
 
 
 @app.route('/')
 def home():
     """
-    Root endpoint to return a welcome message.
+    Endpoint to display a welcome message.
     """
     return "Welcome to the Flask API!"
 
@@ -17,7 +17,7 @@ def home():
 @app.route('/data')
 def get_usernames():
     """
-    Endpoint to retrieve all usernames stored in the API.
+    Endpoint to return a JSON object.
     """
     usernames = list(users.keys())
     return jsonify(usernames), 200
@@ -26,7 +26,7 @@ def get_usernames():
 @app.route('/status')
 def status():
     """
-    Endpoint to check the status of the API.
+    Endpoint to return a simple status message.
     """
     return "OK"
 
@@ -34,7 +34,7 @@ def status():
 @app.route('/users/<username>')
 def get_user(username):
     """
-    Endpoint to retrieve user details by username.
+    Endpoint to return the details of a user.
     """
     user = users.get(username)
     if user:
@@ -42,37 +42,34 @@ def get_user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
-    if username is None:
-        return jsonify({"error": "Username is required"}), 400
-
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
     """
-    Endpoint to add a new user to the users dictionary.
+    Endpoint to add a new user.
     """
-    new_user_data = request.get_json()
+    new_user = request.get_json()
 
-    # Validate that the 'username' field is provided
-    username = new_user_data.get("username")
+    # Check if the username is provided.
+    username = new_user.get("username")
     if not username:
         return jsonify({"error": "Username is required"}), 400
 
-    # Check if the username already exists
+    # Check if the user already exists.
     if username in users:
         return jsonify({"error": "User already exists"}), 400
 
-    # Add the new user to the users dictionary
+    # Add the new user to the dictionary.
     users[username] = {
-        "username": new_user_data.get('username'),
-        "name": new_user_data.get('name'),
-        "age": new_user_data.get('age'),
-        "city": new_user_data.get('city')
+        "username": new_user.get("username"),
+        "name": new_user.get("name"),
+        "age": new_user.get("age"),
+        "city": new_user.get("city")
     }
 
     return jsonify({"message": "User added", "user": users[username]}), 201
 
 
 if __name__ == "__main__":
-    # Run the Flask application
+    # Run the Flask app.
     app.run()
