df10b4041d00c010697e79e9f862c66377da460f
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-10-13 22:17:30 +0200

    Add documentation

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,51 +1,56 @@
 from flask import Flask, jsonify, request, json
-
+"""Create a Flask API with the following endpoints:
+- GET /: return a welcome message.
+- GET /data: return the list of usernames.
+- GET /status: return the status of the API.
+- GET /users/<username>: return the profile of a user.
+- POST /add_user: add a new user to the API.
+"""
 
 app = Flask(__name__)
 
-
+# The dictionary to store the users' profiles.
 users = {}
 
 
 @app.route('/')
 def home():
-    """Return a welcome message for the API."""
+    """Endpoint to return a welcome message."""
     return "Welcome to the Flask API!"
 
 
 @app.route('/data')
 def get_username():
-    """Return a list of usernames."""
+    """Endpoint to return the list of usernames."""
     return jsonify(list(users.keys()))
 
 
 @app.route('/status')
 def status():
-    """Return the status of the API."""
+    """Endpoint to return the status of the API."""
     return "OK"
 
 
 @app.route('/users/<username>')
 def name(username):
-    """Return user profile for the given username."""
-    profile = users.get(username)
-    if profile:
-        return jsonify(profile)
+    """Endpoint to return the profile of a user."""
+    if users.get(username):
+        return jsonify(users.get(username))
     else:
         return jsonify({"error": "User not found"}), 404
 
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    """Add a new user to the users dictionary."""
-    user_data = request.get_json()
-    username = user_data.get('username')
+    """Endpoint to add a new user."""
+    new_user = request.get_json()
+    username = new_user.get('username')
 
     if not username:
         return jsonify({"error": "Username is required"}), 400
     else:
-        users[username] = user_data
-        return jsonify({"message": "User added", "user": user_data}), 201
+        users[username] = new_user
+        return jsonify({"message": "User added", "user": new_user}), 201
 
 
 if __name__ == "__main__":
