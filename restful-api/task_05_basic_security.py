efcd2354477b8707b780f485947055c1eeaa918c
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-10-15 12:45:28 +0200

    Try to fix it

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -58,8 +58,9 @@ jwt = JWTManager(app)
 # Create a route to login and get a JWT token
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
+    data = request.get_json()
+    username = data.get("username", None)
+    password = data.json.get("password", None)
     if not username or not password:
         return jsonify({"message": "Missing username or password"}), 400
     user = users.get(username)
