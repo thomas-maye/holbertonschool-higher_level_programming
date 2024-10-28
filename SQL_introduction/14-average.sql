3b6156c8ab93ee2048b48b1f24599f8c31cfb051
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-10-28 16:33:40 +0100

    task 14 to 16 and the README.md file

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Create a script that computes the average score of all records in the table second_table of the database hbtn_0c_0
+-- The resulat column name should be average
+SELECT AVG(score) AS average FROM second_table;
