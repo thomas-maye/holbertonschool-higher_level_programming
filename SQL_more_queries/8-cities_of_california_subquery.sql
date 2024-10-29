5a88ea21969174d294820665255b12391814af16
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-10-29 17:02:16 +0100

    Try to fix it

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,10 @@
 -- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT * FROM cities WHERE state_id = 1 ORDER BY id ASC;
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+    LIMIT 1
+)
+ORDER BY cities.id ASC;
