02d72aff62d222e02bed30825213212bcaf4898d
Thomas Maye <thomas.maye.33@gmail.com>
2024-10-30 16:38:24 +0100

    Update all the files with the shebang, and test for the task 1

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Create a connection to the database and retrieve all states"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """The code is not executed when the file is imported"""
+    db = MySQLdb.connect(  # Connect to the database
+        host="localhost",  # My host, usually localhost
+        user=sys.argv[1],  # The user name specified in the command line
+        passwd=sys.argv[2],  # The password specified in the command line
+        db=sys.argv[3],  # The database name specified in the command line
+        port=3306)  # The port number to connect to MySQL
+cursor = db.cursor()  # Create a cursor object
+cursor.execute("SELECT * FROM states ORDER BY id ASC")  # Execute the query
+states = cursor.fetchall()  # Fetch all the rows in a list of lists
+for state in states:  # Print the states in the list of lists states
+    print(state)  # Print the state
+cursor.close()  # Close the cursor
+db.close()  # Close the database
