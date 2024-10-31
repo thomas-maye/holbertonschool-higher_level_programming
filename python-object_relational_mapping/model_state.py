f5196b8a9424b962fa1866fab8a3fdda1def0b9b
Thomas Maye <thomas.maye.33@gmail.com>
2024-10-31 15:32:40 +0100

    Add SQLAlchemy model for State and database creation script

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Start link class to table in database"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base
+import sys
+
+Base = declarative_base()
+
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
