5bfe4b07e834862b365cefd68e2d79ef6c207433
Thomas Maye <thomas.maye.33@gmail.com>
2024-10-31 16:15:12 +0100

    Fix formatting and improve readability in state fetch script

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """Fetch all states from the database
-    
+
     Usage: ./7-model_state_fetch_all.py
     <mysql username>
     <mysql password>
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         mysql_username (str): MySQL username
         mysql_password (str): MySQL password
         database_name (str): MySQL database name
-    
+
     Returns:
         The states in the database
     """
@@ -31,7 +31,8 @@ if __name__ == "__main__":
     mysql_username, mysql_password, database_name = sys.argv[1:4]
     # Create a connection to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(mysql_username, mysql_password, database_name))
+                           .format(mysql_username, mysql_password,
+                                   database_name))
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
     # Create a Session
